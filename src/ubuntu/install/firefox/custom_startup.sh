bc313dd5cfb0bcdad92791301d4a31901a4b2b2f
ubuntu <s2s>
2025-05-17 22:56:37 +0000

    moved symlink code block to reachable place lol

--- src/ubuntu/install/firefox/custom_startup.sh
+++ src/ubuntu/install/firefox/custom_startup.sh
@@ -35,6 +35,11 @@ if [ -f /opt/VirtualGL/bin/vglrun ] && [ ! -z "${KASM_EGL_CARD}" ] && [ ! -z "${
     START_COMMAND="/opt/VirtualGL/bin/vglrun -d ${KASM_EGL_CARD} $START_COMMAND"
 fi
 
+create_symlink() {
+    rm -rf $HOME/Downloads
+    ln -sf $HOME/safe-storage $HOME/Downloads
+}
+
 kasm_exec() {
     if [ -n "$OPT_URL" ] ; then
         URL=$OPT_URL
@@ -47,6 +52,7 @@ kasm_exec() {
     if [ -n "$URL" ] ; then
         /usr/bin/filter_ready
         /usr/bin/desktop_ready
+        create_symlink
         bash ${MAXIMIZE_SCRIPT} &
         $START_COMMAND $ARGS $OPT_URL
     else
@@ -71,6 +77,7 @@ kasm_startup() {
             then
                 /usr/bin/filter_ready
                 /usr/bin/desktop_ready
+                create_symlink
                 set +e
                 bash ${MAXIMIZE_SCRIPT} &
                 $START_COMMAND $ARGS $URL
@@ -84,18 +91,11 @@ kasm_startup() {
 
 }
 
-create_symlink() {
-    rm -rf $HOME/Downloads
-    ln -sf $HOME/safe-storage $HOME/Downloads
-}
-
 if [ -n "$GO" ] || [ -n "$ASSIGN" ] ; then
     kasm_exec
 else
     kasm_startup
 fi
-
-create_symlink
 
 
 
